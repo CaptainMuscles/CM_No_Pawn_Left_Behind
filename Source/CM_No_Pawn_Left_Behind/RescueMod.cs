394066db15c2b44efdf334b81475ec79132c9691
NathanHarbour <nathanharbour@gmail.com>
2021-02-19 12:15:39 -0600

    Initial submission

---
+++ Source/CM_No_Pawn_Left_Behind/RescueMod.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace CM_No_Pawn_Left_Behind
+{
+    public class RescueMod : Mod
+    {
+        private static RescueMod _instance;
+        public static RescueMod Instance => _instance;
+
+        public static RescueModSettings settings;
+
+        public RescueMod(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("CM_No_Pawn_Left_Behind");
+            harmony.PatchAll();
+
+            _instance = this;
+            settings = GetSettings<RescueModSettings>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "No Pawn Left Behind";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            settings.DoSettingsWindowContents(inRect);
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            settings.UpdateSettings();
+        }
+    }
+}
---
+++ Source/CM_No_Pawn_Left_Behind/RescueModSettings.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace CM_No_Pawn_Left_Behind
+{
+    public class RescueModSettings : ModSettings
+    {
+        public bool logPriorities = false;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref logPriorities, "logPriorities", false);
+        }
+
+        public void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.ColumnWidth = (inRect.width - 34f) / 2f;
+
+            listing_Standard.Begin(inRect);
+
+            listing_Standard.CheckboxLabeled("CM_No_Pawn_Left_Behind_Settings_Log_Priorities_Label".Translate(), ref logPriorities);
+
+            listing_Standard.End();
+        }
+
+        public void UpdateSettings()
+        {
+
+        }
+    }
+}
